47221ea2907d4c5738183f74d3ff68b01b7162bd
playerdecuple <playerdecuple@gmail.com>
2020-08-31 12:01:15 +0900

    Use 'StandardCharsets'

--- src/main/java/com/DecupleProject/Core/Util/GetJSON.java
+++ src/main/java/com/DecupleProject/Core/Util/GetJSON.java
@@ -4,19 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class GetJSON {
 
     public GetJSON() {}
 
     public String getJsonByUrl(String urlS) throws Exception {
-        BufferedReader br = null;
+        BufferedReader br;
 
         URL url = new URL(urlS);
 
         HttpURLConnection uC = (HttpURLConnection) url.openConnection();
         uC.setRequestMethod("GET");
-        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), "UTF-8"));
+        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), StandardCharsets.UTF_8));
 
         StringBuilder result = new StringBuilder();
         String line;
@@ -28,14 +29,15 @@ public class GetJSON {
         return result.toString();
     }
 
+    /* Never used code yet.
     public String postJsonByUrl(String urlS) throws Exception {
-        BufferedReader br = null;
+        BufferedReader br;
 
         URL url = new URL(urlS);
 
         HttpURLConnection uC = (HttpURLConnection) url.openConnection();
         uC.setRequestMethod("POST");
-        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), "UTF-8"));
+        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), StandardCharsets.UTF_8));
 
         StringBuilder result = new StringBuilder();
         String line;
@@ -46,15 +48,16 @@ public class GetJSON {
 
         return result.toString();
     }
+     */
 
     public String getJsonByUrlForUserMode(String urlS) throws Exception {
-        BufferedReader br = null;
+        BufferedReader br;
 
         URL url = new URL(urlS);
 
         HttpURLConnection uC = (HttpURLConnection) url.openConnection();
         uC.setRequestProperty("User-Agent", "Mozilla");
-        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), "UTF-8"));
+        br = new BufferedReader(new InputStreamReader(uC.getInputStream(), StandardCharsets.UTF_8));
 
         StringBuilder result = new StringBuilder();
         String line;
