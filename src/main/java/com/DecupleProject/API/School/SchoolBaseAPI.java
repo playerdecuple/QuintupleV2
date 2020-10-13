47438d76fffe93f3fe1e4f58ce2e6184db05755c
playerdecuple <playerdecuple@gmail.com>
2020-10-14 03:54:16 +0900

    Edited imported classes

--- src/main/java/com/DecupleProject/API/School/SchoolBaseAPI.java
+++ src/main/java/com/DecupleProject/API/School/SchoolBaseAPI.java
@@ -6,14 +6,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.net.URLEncoder;
 
 public class SchoolBaseAPI {
