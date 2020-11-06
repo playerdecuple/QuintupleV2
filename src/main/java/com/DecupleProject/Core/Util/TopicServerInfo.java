e236b093c1bf59a7c3e218653c6f2b7b838bcf1f
playerdecuple <playerdecuple@gmail.com>
2020-11-06 18:36:31 +0900

    Update topic editor

--- src/main/java/com/DecupleProject/Core/Util/TopicServerInfo.java
+++ src/main/java/com/DecupleProject/Core/Util/TopicServerInfo.java
@@ -16,12 +16,12 @@ public class TopicServerInfo {
         if (mc.isTestMode())
             return;
 
-        int serverNow = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getMemberCount();
-        int serverChannel = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getTextChannels().size();
-        int serverBoostCount = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostCount();
-        int serverBoostTier = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostTier().getKey();
-
         exec.scheduleAtFixedRate(() -> {
+            int serverNow = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getMemberCount();
+            int serverChannel = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getTextChannels().size();
+            int serverBoostCount = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostCount();
+            int serverBoostTier = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostTier().getKey();
+
             Objects.requireNonNull(DefaultListener.jda.getTextChannelById("699439025479745556"))
                     .getManager().setTopic("아무 주제에 관한 대화방이에요. (:person_gesturing_ok: 서버 인원 | " + serverNow +
                     "명 :thought_balloon: 채널 갯수 | " + serverChannel + "개 :moneybag: 부스트 횟수 | " + serverBoostCount + "번[" + serverBoostTier + " 단계])").queue();
