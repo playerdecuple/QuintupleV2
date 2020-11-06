dc8b9b27319470eb81c76ba8077b75d8fcfa4d5b
playerdecuple <playerdecuple@gmail.com>
2020-11-06 18:37:22 +0900

    Update topic edit target

--- src/main/java/com/DecupleProject/Core/Util/TopicServerInfo.java
+++ src/main/java/com/DecupleProject/Core/Util/TopicServerInfo.java
@@ -22,7 +22,7 @@ public class TopicServerInfo {
             int serverBoostCount = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostCount();
             int serverBoostTier = Objects.requireNonNull(DefaultListener.jda.getGuildById("615469086616584193")).getBoostTier().getKey();
 
-            Objects.requireNonNull(DefaultListener.jda.getTextChannelById("699439025479745556"))
+            Objects.requireNonNull(DefaultListener.jda.getTextChannelById("774204942557642772"))
                     .getManager().setTopic("아무 주제에 관한 대화방이에요. (:person_gesturing_ok: 서버 인원 | " + serverNow +
                     "명 :thought_balloon: 채널 갯수 | " + serverChannel + "개 :moneybag: 부스트 횟수 | " + serverBoostCount + "번[" + serverBoostTier + " 단계])").queue();
         }, 0, sleepSec, TimeUnit.SECONDS);
